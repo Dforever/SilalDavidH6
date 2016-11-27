5fc1aba5e716021754818de8a1af4d770d51d8d8
David Silal <dsilal@albany.edu>
2016-11-27 13:28:16 -0500

    Public and private classes for Node

---
+++ Node.java
@@ -0,0 +1,10 @@
+public class Node {
+	//left node
+    private Node left;
+    //right node
+    private Node right;
+    //contents of the node
+    private String contents;
+    //count of instances
+    private int count;
+
